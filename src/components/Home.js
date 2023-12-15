be89304f4aa7f8b018d797a4c9dafb6e39168510
Bettenoch <bettenoch254@gmail.com>
2023-12-15 23:23:07 +0500

    import navigation bar into home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,16 @@
+import NavigationBar from './NavigationBar';
+
 function Home() {
   return (
     <>
-      <h1>Home page</h1>
-      <h1>Navigation bar</h1>
+      <div className='flex flex-row w-[100dvw]'>
+        <div className='flex w-[25] bg-slate-400'>
+          <NavigationBar />
+        </div>
+        <div className='flex w-[75] bg-slate-950'>
+          <h1>home component</h1>
+        </div>
+      </div>
     </>
   );
 }
