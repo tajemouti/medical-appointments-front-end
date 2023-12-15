f6c4610cdf81ac5549c9415818de44973ea7ebf1
Bettenoch <bettenoch254@gmail.com>
2023-12-16 00:09:33 +0500

    create a navigation bar and add style

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,17 +2,17 @@ import logo from '../images/logo.svg'
 function NavigationBar() {
   return (
     <>
-      <div className='flex flex-col p-4 justify-between items-center h-[100dvh] bg-white w-full drop-shadow-md'>
+      <div className='flex flex-col pt-4 pb-4 justify-between items-center h-[100dvh] bg-white w-full drop-shadow-md'>
         <div>
           <img src={logo} alt="" />
         </div>
-        <nav>
-          <ul>
-            <li>Doctors</li>
-            <li>Book Appointment</li>
-            <li>My Appointments</li>
-            <li>Add Doctor</li>
-            <li>Delete Doctor</li>
+        <nav className='w-full'>
+          <ul className='flex flex-col'>
+            <li className='flex p-4 w-full bg-[#97BF0F] text-white text-sm font-medium cursor-pointer border-b border-gray-200'>Doctors</li>
+            <li className='flex p-4 w-full  text-sm font-medium cursor-pointer border-b border-gray-200 hover:bg-gray-100'>Book Appointment</li>
+            <li className='flex p-4 w-full  text-sm font-medium cursor-pointer border-b border-gray-200 hover:bg-gray-100'>My Appointments</li>
+            <li className='flex p-4 w-full  text-sm font-medium cursor-pointer border-b border-gray-200 hover:bg-gray-100'>Add Doctor</li>
+            <li className='flex p-4 w-full  text-sm font-medium cursor-pointer border-b border-gray-200 hover:bg-gray-100'>Delete Doctor</li>
           </ul>
         </nav>
         <footer>
