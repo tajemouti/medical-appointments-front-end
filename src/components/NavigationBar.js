6f6285806c73a068e86eff4286f49f353f0b603f
Bettenoch <bettenoch254@gmail.com>
2023-12-15 23:36:34 +0500

    add logo to the navigation bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,7 +1,24 @@
+import logo from '../images/logo.svg'
 function NavigationBar() {
   return (
     <>
-    <h1>Navigation bar is working </h1>
+      <div>
+        <div>
+          <img src={logo} alt="" />
+        </div>
+        <nav>
+          <ul>
+            <li>Doctors</li>
+            <li>Book Appointment</li>
+            <li>My Appointments</li>
+            <li>Add Doctor</li>
+            <li>Delete Doctor</li>
+          </ul>
+        </nav>
+        <footer>
+          <h1>footer</h1>
+        </footer>
+      </div>
     </>
   )
 }
