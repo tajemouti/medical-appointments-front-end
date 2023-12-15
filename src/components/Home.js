1d19e05c800677799595cd44271522c311281c6d
tajemouti <tajemouti@gmail.com>
2023-12-15 20:20:07 +0100

    Fix linters' errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,11 @@ import NavigationBar from './NavigationBar';
 function Home() {
   return (
     <>
-      <div className='flex flex-row w-[100dvw]'>
-        <div className='flex w-[15%]'>
+      <div className="flex flex-row w-[100dvw]">
+        <div className="flex w-[15%]">
           <NavigationBar />
         </div>
-        <div className='flex w-[85%] bg-white'>
+        <div className="flex w-[85%] bg-white">
           <h1>home component</h1>
         </div>
       </div>
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,39 +1,39 @@
-import logo from '../images/logo.svg'
-import fb from '../images/fb.svg'
-import google from '../images/google.svg'
-import x from '../images/x.svg'
-import github from '../images/github.svg'
-import linkedin from '../images/linkedin.svg'
+import logo from '../images/logo.svg';
+import fb from '../images/fb.svg';
+import google from '../images/google.svg';
+import x from '../images/x.svg';
+import github from '../images/github.svg';
+import linkedin from '../images/linkedin.svg';
 
 function NavigationBar() {
   return (
     <>
-      <div className='flex flex-col pt-4 pb-1 justify-between items-center h-[100dvh] bg-white w-full drop-shadow-md'>
+      <div className="flex flex-col pt-4 pb-1 justify-between items-center h-[100dvh] bg-white w-full drop-shadow-md">
         <div>
           <img src={logo} alt="" />
         </div>
-        <nav className='w-full'>
-          <ul className='flex flex-col'>
-            <li className='flex p-4 w-full bg-[#97BF0F] text-white text-sm font-medium cursor-pointer border-b border-gray-200'>Doctors</li>
-            <li className='flex p-4 w-full  text-sm font-medium cursor-pointer border-b border-gray-200 hover:bg-gray-100'>Book Appointment</li>
-            <li className='flex p-4 w-full  text-sm font-medium cursor-pointer border-b border-gray-200 hover:bg-gray-100'>My Appointments</li>
-            <li className='flex p-4 w-full  text-sm font-medium cursor-pointer border-b border-gray-200 hover:bg-gray-100'>Add Doctor</li>
-            <li className='flex p-4 w-full  text-sm font-medium cursor-pointer border-b border-gray-200 hover:bg-gray-100'>Delete Doctor</li>
+        <nav className="w-full">
+          <ul className="flex flex-col">
+            <li className="flex p-4 w-full bg-[#97BF0F] text-white text-sm font-medium cursor-pointer border-b border-gray-200">Doctors</li>
+            <li className="flex p-4 w-full  text-sm font-medium cursor-pointer border-b border-gray-200 hover:bg-gray-100">Book Appointment</li>
+            <li className="flex p-4 w-full  text-sm font-medium cursor-pointer border-b border-gray-200 hover:bg-gray-100">My Appointments</li>
+            <li className="flex p-4 w-full  text-sm font-medium cursor-pointer border-b border-gray-200 hover:bg-gray-100">Add Doctor</li>
+            <li className="flex p-4 w-full  text-sm font-medium cursor-pointer border-b border-gray-200 hover:bg-gray-100">Delete Doctor</li>
           </ul>
         </nav>
-        <footer className='flex flex-col gap-4 w-full pt-4 pb-2 pl-4 pr-4'>
-          <ul className=' justify-start items-end gap-3.5 inline-flex'>
-            <li className='w-6 h-6 relative'><img src={fb} alt="" /></li>
-            <li className='w-6 h-6 relative'><img src={x} alt="" /></li>
-            <li className='w-6 h-6 relative'><img src={google} alt="" /></li>
-            <li className='w-6 h-6 relative'><img src={github} alt="" /></li>
-            <li className='w-6 h-6 relative'><img src={linkedin} alt="" /></li>
+        <footer className="flex flex-col gap-4 w-full pt-4 pb-2 pl-4 pr-4">
+          <ul className=" justify-start items-end gap-3.5 inline-flex">
+            <li className="w-6 h-6 relative"><img src={fb} alt="" /></li>
+            <li className="w-6 h-6 relative"><img src={x} alt="" /></li>
+            <li className="w-6 h-6 relative"><img src={google} alt="" /></li>
+            <li className="w-6 h-6 relative"><img src={github} alt="" /></li>
+            <li className="w-6 h-6 relative"><img src={linkedin} alt="" /></li>
           </ul>
           <p className="text-gray-300 text-xs font-medium font-['Inter'] leading-[18px] tracking-[2.88px]">@ 2023 Health Clinic</p>
         </footer>
       </div>
     </>
-  )
+  );
 }
 
 export default NavigationBar;
