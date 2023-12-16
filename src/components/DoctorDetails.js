66d59dd92e952059357d82866ea8243af2c2992d
wacoo <wabaham9@gmail.com>
2023-12-17 00:31:54 +0100

    Style doctor details section

--- src/components/DoctorDetails.js
+++ src/components/DoctorDetails.js
@@ -16,12 +16,16 @@ function DoctorDetails({ doctor }) {
       <div className="flex items-end ">
         <img src={doctor.picture} alt="" />
       </div>
-      <div>
-        <h1>{doctor.name}</h1>
-        <h2>{doctor.speciality}</h2>
-        <h3>{doctor.address}</h3>
+      <div className="flex flex-col justify-between self-end pb-12 pr-8 h-[70%]">
+        <div className="flex flex-col gap-2 items-end">
+          <h1 className="text-slate-800 text-4xl font-bold font-['Inter'] leading-[72px]">{doctor.name}</h1>
+          <h2 className="w-[383px] text-right text-gray-400 text-xl font-normal font-['Inter'] leading-[44px]">{doctor.speciality}</h2>
+          <h3 className="w-[383px] text-right text-gray-400 text-sm font-normal font-['Inter'] leading-[44px]">{doctor.address}</h3>
+        </div>
+        <button>
+          Book Appointment
+        </button>
       </div>
-      <button>Book Appointment</button>
     </div>
   );
 }
