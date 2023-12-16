ae2841869aa132bc875e47efe2134b4fbcd0e79a
wacoo <wabaham9@gmail.com>
2023-12-17 00:21:28 +0100

    Add back button

--- src/components/DoctorDetails.js
+++ src/components/DoctorDetails.js
@@ -1,11 +1,23 @@
 import PropTypes from 'prop-types';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 function DoctorDetails({ doctor }) {
+  if (!doctor) {
+    return null;
+  }
+
   return (
     <div>
-      <h1>{doctor.name}</h1>
-      <h2>{doctor.speciality}</h2>
-      <h3>{doctor.address}</h3>
+      <div>
+        <button>
+          <ArrowBackIcon />
+        </button>
+      </div>
+      <div>
+        <h1>{doctor.name}</h1>
+        <h2>{doctor.speciality}</h2>
+        <h3>{doctor.address}</h3>
+      </div>
     </div>
   );
 }
